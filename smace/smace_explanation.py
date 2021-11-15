9a43e49b16d04d2ebd186edcb20eb4803f496a5b
gianluigilopardo <gianluigilopardo@gmail.com>
2021-11-15 17:18:06 +0100

    new experiments

--- smace/smace_explanation.py
+++ smace/smace_explanation.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 class SmaceExplanation:
     """ Object returned by explainers. """
+
     def __init__(self, example, exp, r, phi):
         """ Create a new Explanation.
 
@@ -19,7 +20,7 @@ class SmaceExplanation:
         self.rule = r
         self.models = phi
 
-    def table(self, num_features=15):
+    def table(self, num_features=5):
         # return dataframe with overall contributions
         e = self.exp
         xi = self.example
@@ -29,11 +30,12 @@ class SmaceExplanation:
         exp_table = pd.DataFrame(exp_table)
         return exp_table.reindex(exp_table.Contribution.abs().sort_values(ascending=False).index)[:num_features]
 
-    def bar(self, num_features=15):
+    def bar(self, num_features=5):
         # return bar plot with overall contributions
-        return self.table(num_features).Contribution.plot.barh()
+        return self.table().Contribution.reindex(self.table().Contribution.abs().
+                                                 sort_values(ascending=True).index)[-num_features:].plot.barh()
 
-    def rule_table(self, num_features=15):
+    def rule_table(self, num_features=5):
         # return dataframe with rule contributions
         e = self.rule
         exp_table = pd.DataFrame(index=list(e.keys()))
@@ -41,11 +43,12 @@ class SmaceExplanation:
         exp_table = pd.DataFrame(exp_table)
         return exp_table.reindex(exp_table.Contribution.abs().sort_values(ascending=False).index)[:num_features]
 
-    def rule_bar(self, num_features=15):
+    def rule_bar(self, num_features=5):
         # return bar plot with rule contributions
-        return self.rule_table(num_features).plot.barh()
+        return self.rule_table().Contribution.reindex(self.table().Contribution.abs().
+                                                      sort_values(ascending=True).index)[-num_features:].plot.barh()
 
-    def model_table(self, model_name, num_features=15):
+    def model_table(self, model_name, num_features=5):
         # return dataframe with model contributions
         e = self.models[model_name]
         exp_table = pd.DataFrame(index=list(e.keys()))
@@ -55,4 +58,6 @@ class SmaceExplanation:
 
     def model_bar(self, model_name, num_features=15):
         # return bar plot with model contributions
-        return self.model_table(model_name, num_features).plot.barh()
+        return self.model_table(model_name).Contribution.reindex(self.table().Contribution.abs().
+                                                                   sort_values(ascending=True).index)[
+               -num_features:].plot.barh()
